58d8bd6b29fc432ce75aba0e54c691c084282514
richarlyson <richarluson@gmail.com>
2026-05-10 23:10:39 -0300

    aula 13 - feat: criando o css global

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import "./styles/theme.css";
+import "./styles/global.css";
 export function App() {
   return (
     <div>
-      <h1>hola mundo do app!</h1>;
+      <h1>ola mundo do app!</h1>;
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
         perspiciatis et repellat commodi odio, quibusdam laborum quis ipsum quo

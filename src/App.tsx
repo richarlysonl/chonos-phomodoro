2015d6eb104a320cb48d4f58a177cbc4ddc26efc
richarlyson <richarluson@gmail.com>
2026-05-17 14:23:26 -0300

    aula 20 - criando conteiners

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Heading } from "./components/Heading";
-import { TimerIcon } from "lucide-react";
 export function App() {
   return (
-    <div>
-      <Heading>
-        Ola mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-        perspiciatis et repellat commodi odio, quibusdam laborum quis ipsum quo
-        perferendis saepe mollitia accusantium debitis quam deleniti enim eaque
-        id in!
-      </p>
+    <div className="container">
+      <div className="content">
+        <section>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          iusto animi nisi a dolore sint quas dicta unde blanditiis, veritatis
+          quia cumque reprehenderit cum quibusdam consequatur autem saepe
+          quaerat. Molestiae.
+        </section>
+      </div>
     </div>
   );
 }
